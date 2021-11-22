9f304bfbb9eed1714ff505b60ba3cc1e049184c0
Shaurya Mittal <mittalshaurya004@gmail.com>
2021-11-22 20:37:51 +0530

    Top Bar

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,25 @@
 import UI.*;
 import Utils.*;
+import java.awt.Toolkit;
+import java.awt.Color;
 import java.awt.event.*;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main implements ActionListener{
 	public static void main(String[] args) {
 		CWindow window = new CWindow("C-Score");
+		Toolkit tk = Toolkit.getDefaultToolkit();
 		window.SetMaximized();
 		CPanel panel = new CPanel();
+		JPanel topBar = new JPanel();
+		topBar.setLayout(new BoxLayout(topBar, BoxLayout.X_AXIS));
+		topBar.setBounds(0, 0, tk.getScreenSize().width, 30);
 		CButton newMatch = new CButton(0,0,100,25,"New Match");
-
+		topBar.setBackground(new Color(200,200,200));
+		
 		newMatch.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				if(e.getActionCommand() == "New Match"){
@@ -19,7 +30,8 @@ public class Main implements ActionListener{
 				}
 			}
 		});
-		panel.add(newMatch);
+		topBar.add(newMatch);
+		panel.add(topBar);
 		window.add(panel);
 		window.setVisible(true);
 	}
--- src/UI/CButton.java
+++ src/UI/CButton.java
@@ -4,7 +4,6 @@ import javax.swing.JButton;
 
 public class CButton extends JButton{
 	public CButton(int x, int y, int width, int height, String text) {
-		setContentAreaFilled(false);
 		setText(text);
 		setBounds(x, y, width, height);
 	}
